5c3922abe206ef6190e2195e168dd80fed4cbc62
Ramon A.C.Vega <raymonchu40@gmail.com>
2026-03-12 18:06:01 +0100

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = window.APP_CONFIG || {
-  SUPABASE_URL:"https://bpnqtswjwjclstsectfk.supabase.co", 
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwbnF0c3dqd2pjbHN0c2VjdGZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMjE4NTQsImV4cCI6MjA4ODg5Nzg1NH0.sHRy2hAF-8cEkfJU-ppOwenZPe_jPPVX7Fp0Jfej3ks" 
-};
