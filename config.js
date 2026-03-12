02b1626f69d51a46cb733c6ce9f23b22ae11a3fe
Ramon A.C.Vega <raymonchu40@gmail.com>
2026-03-12 15:31:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = window.APP_CONFIG || {
-  SUPABASE_URL:"https://qzwauzyeewmgetkrhypz.supabase.co", 
+  SUPABASE_URL:"bpnqtswjwjclstsectfk", 
   SUPABASE_ANON_KEY: "sb_publishable_TMWq0-K-NwmGDXhqxsQlEA_bt1O4F6q" 
 };
