b2edc1023279b46384a23533987bc75625273a8f
Ramon A.C.Vega <raymonchu40@gmail.com>
2026-03-12 15:33:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = window.APP_CONFIG || {
-  SUPABASE_URL:"bpnqtswjwjclstsectfk", 
-  SUPABASE_ANON_KEY: "sb_publishable_TMWq0-K-NwmGDXhqxsQlEA_bt1O4F6q" 
+  SUPABASE_URL:"https://bpnqtswjwjclstsectfk.supabase.co", 
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwbnF0c3dqd2pjbHN0c2VjdGZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMjE4NTQsImV4cCI6MjA4ODg5Nzg1NH0.sHRy2hAF-8cEkfJU-ppOwenZPe_jPPVX7Fp0Jfej3ks" 
 };
