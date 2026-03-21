865d021a41a6fde97f9b5f90e6949cff8428c10e
Ramon A.C.Vega <raymonchu40@gmail.com>
2026-03-21 13:07:55 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,52 +1,5 @@
-const CACHE_NAME = "inventario-pro-v11";
-
-const APP_SHELL = [
-  "./",
-  "./index.html",
-  "./styles.css?v=11",
-  "./app.js?v=11",
-  "./config.js?v=11",
-  "./manifest.json",
-  "./logo-banquetes.png",
-  "./Banner-Banquetes.png",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
-  );
-  self.skipWaiting();
-});
-
+self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const responseClone = response.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, responseClone);
-        });
-        return response;
-      })
-      .catch(() => caches.match(event.request).then(cached => cached || caches.match("./index.html")))
-  );
-});
+self.addEventListener("fetch", () => {});
